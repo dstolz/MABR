d8301b6e7d8e368587bdd17f563531e54415b9e7
daniel <daniel.stolzberg@gmail.com>
2019-06-03 13:13:10 -0400

    fixed device max channel count

--- +abr/@ABR/setupAudioChannels.m
+++ +abr/@ABR/setupAudioChannels.m
@@ -17,13 +17,13 @@ ch = inputdlg({'Stimulus Output','Recording Input','Loop-Back Output','Loop-Back
 
 if isempty(ch), return; end
 
-% info = obj.APR.info;
-% 
-% maxIn  = info.MaximumRecorderChannels;
-% maxOut = info.MaximumPlayerChannels;
-% TO DO: FIX ME
-maxIn = 6;
-maxOut = 8;
+apr = audioPlayerRecorder;
+info = apr.info;
+delete(apr);
+
+maxIn  = info.MaximumRecorderChannels;
+maxOut = info.MaximumPlayerChannels;
+
 ch = cellfun(@str2double,ch);
 
 if any(ch < 1) || any(ch([1 3]) > maxIn) || any(ch([2 4]) > maxOut)
