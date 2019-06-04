e36233df79493d658bca301a2db56d64521d3ed7
daniel <daniel.stolzberg@gmail.com>
2019-06-04 18:02:39 -0400

    define mean along first dim only

--- +abr/@ControlPanel/partition_corr.m
+++ +abr/@ControlPanel/partition_corr.m
@@ -8,16 +8,16 @@ n = min([size(preSweep,1) size(postSweep,1)]);
 m = round(n/2);
 i = randperm(n);
 
-preMean1  = mean(preSweep(i(1:m),:))';
-preMean2  = mean(preSweep((i(m+1:end)),:))';
-postMean1 = mean(postSweep(i(1:m),:))';
-postMean2 = mean(postSweep(i(m+1:end),:))';
+preMean1  = mean(preSweep(i(1:m),:),1)';
+preMean2  = mean(preSweep((i(m+1:end)),:),1)';
+postMean1 = mean(postSweep(i(1:m),:),1)';
+postMean2 = mean(postSweep(i(m+1:end),:),1)';
 
 % compute auto and cross correlation between preSweep and postSweep stimulus means
 R = corrcoef([preMean1 preMean2 postMean1 postMean2]);
 
 Rpre   = R(2,1);
-Rcross = mean(R(sub2ind([4 4],[3 3 4 4],[1 2 1 2])));
+Rcross = mean(R(sub2ind([4 4],[3 3 4 4],[1 2 1 2])),1);
 Rpost  = R(4,3);
 
 R = abs([Rpre Rcross Rpost]);
