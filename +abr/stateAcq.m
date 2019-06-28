39043d0f4f1c7f9fc51e1b46621c450000db54ca
daniel <daniel.stolzberg@gmail.com>
2019-06-28 10:58:51 -0400

    ordered enums for easier numeric comparison

--- +abr/stateAcq.m
+++ +abr/stateAcq.m
@@ -1,17 +1,17 @@
 classdef stateAcq < int8
     
     enumeration
-        DELETED     (-3)
-        INIT        (-2)
-        ERROR       (-1)
+        INIT        (-1)
         IDLE        (0)
         READY       (1)
-        ACQUIRE (2)
+        ACQUIRE     (2)
         CANCELLED   (3)
         PAUSED      (4)
         COMPLETED   (5)
         ADVANCED    (6)
-        KILLED      (255)
+        ERROR       (125)
+        DELETED     (126)
+        KILLED      (127)
     end
     
 end
