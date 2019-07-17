d8b5c70b1decca9cf10b6263d79c37f7864f1ad9
daniel <daniel.stolzberg@gmail.com>
2019-07-17 13:36:32 -0400

    partition block into odd and even trials instead of randomized selection

--- +abr/@ControlPanel/partition_corr.m
+++ +abr/@ControlPanel/partition_corr.m
@@ -1,17 +1,22 @@
 function R = partition_corr(preSweep,postSweep)
+% Compute the correlation coefficents for data following a stimulus
+% onset (postSweep), preceding a stimulus onset (preSweep), as well as
+% their crosscorrelation.
+%
+% To do this efficiently, just split the data into two datasets, odd sweeps
+% and even sweeps, and then compute the correlation coefficients using
+% corrcoef.
+%
 % Compute Pearson's correlation in a similar fashion to Arnold et al, 1985
 % Arnold, S.A., et al (1985). Objective versus visual detection of the
 % auditory brain stem response. Ear and Hearing, 6(3), 144–150.
 
-% partition the sweeps into two random subsamples
-n = min([size(preSweep,1) size(postSweep,1)]);
-m = round(n/2);
-i = randperm(n);
 
-preMean1  = mean(preSweep(i(1:m),:),1)';
-preMean2  = mean(preSweep((i(m+1:end)),:),1)';
-postMean1 = mean(postSweep(i(1:m),:),1)';
-postMean2 = mean(postSweep(i(m+1:end),:),1)';
+
+preMean1  = mean(preSweep(1:2:end,:),1)';
+preMean2  = mean(preSweep((2:2:end),:),1)';
+postMean1 = mean(postSweep(1:2:end,:),1)';
+postMean2 = mean(postSweep(2:2:end,:),1)';
 
 % compute auto and cross correlation between preSweep and postSweep stimulus means
 R = corrcoef([preMean1 preMean2 postMean1 postMean2]);
