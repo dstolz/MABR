a14b1e6b00efc3fd13a0caae889f349ac72e8acf
daniel <daniel.stolzberg@gmail.com>
2022-02-17 17:05:35 -0500

    check PC support

--- MABR.m
+++ MABR.m
@@ -1,5 +1,10 @@
 function h = MABR(rootDir)
 
+if ~ispc
+    vprintf(0,1,'No support yet for non-Windows operating systems! :(')
+    return
+end
+
 if nargin == 0 || isempty(rootDir)
     s = dbstack('-completenames');
     rootDir = fileparts(s(1).file); 
