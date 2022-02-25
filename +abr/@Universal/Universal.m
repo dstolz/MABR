d1a2d1ba65e99c46dd7bf64644d86c7aabec3266
daniel <daniel.stolzberg@gmail.com>
2022-02-25 13:23:15 -0500

    checking for user32 library causing issues ???

--- +abr/@Universal/Universal.m
+++ +abr/@Universal/Universal.m
@@ -79,16 +79,16 @@ classdef Universal < handle
             obj.infoFile        = fullfile(obj.runtimePath,'info.mat');
             
             
-            try
-                if ~libisloaded('user32')
-                    loadlibrary('C:\WINDOWS\system32\user32.dll','user32.h');
-                end
-                L = calllib('user32', 'GetAsyncKeyState', int32(1)) ~= 0; %#ok<NASGU>
-            catch me
-                vprintf(0,1,me)
-                pth = which('mingw.mlpkginstall');
-                vprintf(0,1,'Must run and install: %s',pth)
-            end
+%             try
+%                 if ~libisloaded('user32')
+%                     loadlibrary('C:\WINDOWS\system32\user32.dll','user32.h');
+%                 end
+%                 L = calllib('user32', 'GetAsyncKeyState', int32(1)) ~= 0; %#ok<NASGU>
+%             catch me
+%                 vprintf(0,1,me)
+%                 pth = which('mingw.mlpkginstall');
+%                 vprintf(0,1,'Must run and install: %s',pth)
+%             end
             
         end
         
