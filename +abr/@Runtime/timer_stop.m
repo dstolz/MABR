0309aa8b4ce241edda5218db584307c7aa4b219f
Daniel <dstolz@umd.edu>
2025-03-04 13:26:20 -0500

    simplify syntax

--- +abr/@Runtime/timer_stop.m
+++ +abr/@Runtime/timer_stop.m
@@ -1,7 +1,7 @@
 function timer_stop(T,event,obj)
 
 if obj.isBackground
-    obj.mapCom.Data.BackgroundState = int8(abr.stateAcq.IDLE);
+    obj.BackgroundState = abr.stateAcq.IDLE;
 else
-    obj.mapCom.Data.ForegroundState = int8(abr.stateAcq.IDLE);
+    obj.ForegroundState = abr.stateAcq.IDLE;
 end
